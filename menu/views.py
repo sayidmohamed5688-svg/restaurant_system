d0f53e71cb0819c30fa155dd8c4fc1b016f036b4
sayid <sayidmohammed5688@gmail.com>
2026-05-10 23:18:17 +0300

    added all data

--- menu/views.py
+++ menu/views.py
@@ -6,8 +6,8 @@ def home(request):
 
 def order(request):
     if request.method == 'POST':
-        customer_name = request.POST['customer_name']
-        item = request.POST['item']
+        customer_name = request.POST.get('customer_name', '')
+        item = request.POST.get('item', '')
         quantity = request.POST.get('quantity', 1)
         
         Order.objects.create(
@@ -18,3 +18,7 @@ def order(request):
         return redirect('home')
     
     return render(request, 'menu/order.html')
+
+def all_orders(request):
+    orders = Order.objects.all()
+    return render(request, 'menu/all_orders.html', {'orders': orders})
