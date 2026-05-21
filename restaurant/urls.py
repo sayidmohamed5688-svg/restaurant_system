25d8fabca115e25c92727c884672dc04b6893c54
sayid <sayidmohammed5688@gmail.com>
2026-05-21 15:28:29 +0300

    added create admin

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from menu import views
 from accounts import views as auth_views
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_admin(request):
+    if not User.objects.filter(username='sayid').exists():
+        User.objects.create_superuser('sayid', 'sayid@email.com', 'sayid1234')
+        return HttpResponse('Admin created!')
+    return HttpResponse('Admin already exists!')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('create-admin/', create_admin),
     path('', views.home, name='home'),
     path('order/', views.order, name='order'),
     path('orders/', views.all_orders, name='all_orders'),
