707fa559f44f087abac0a9cf5457de9458a0bbab
sayid <sayidmohammed5688@gmail.com>
2026-05-23 14:40:02 +0300

    switched to image URL

--- menu/models.py
+++ menu/models.py
@@ -5,7 +5,7 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField(blank=True)
     available = models.BooleanField(default=True)
-    image = models.ImageField(upload_to='menu_images/', blank=True, null=True)
+    image = models.URLField(blank=True, null=True)
 
     def __str__(self):
         return f"{self.name} - ${self.price}"
