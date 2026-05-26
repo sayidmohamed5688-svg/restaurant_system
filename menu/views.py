3e0e1d6a35475319850a64519c99d2b91bf134b0
sayid <sayidmohammed5688@gmail.com>
2026-05-27 01:35:05 +0300

    fc

--- menu/views.py
+++ menu/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import Order, MenuItem, Category, Table, OrderItem
@@ -59,7 +58,7 @@ def search(request):
 def waiter_dashboard(request):
     from django.contrib.auth.models import User
     waiters = User.objects.filter(
-        username__in=['gadhyac', 'faro', 'sakriye', 
+        username__in=['gadhyac', 'faro', 'sakriye',
                       'cabdi_xasan', 'shine', 'sakariye_mxmed']
     )
     return render(request, 'menu/waiter_dashboard.html', {'waiters': waiters})
@@ -77,9 +76,8 @@ def take_order(request, table_number):
     from django.contrib.auth.models import User
     table = Table.objects.get(number=table_number)
     categories = Category.objects.all()
-    menu_items = MenuItem.objects.filter(available=True)
-    
-    # Get waiter from URL parameter
+    menu_items = MenuItem.objects.all()
+
     waiter_username = request.GET.get('waiter', '')
     waiter = None
     if waiter_username:
@@ -87,17 +85,17 @@ def take_order(request, table_number):
             waiter = User.objects.get(username=waiter_username)
         except:
             pass
-    
+
     if request.method == 'POST':
         customer_name = request.POST.get('customer_name', '')
         payment_method = request.POST.get('payment_method', 'cash')
         waiter_username = request.POST.get('waiter_username', '')
-        
+
         try:
             waiter = User.objects.get(username=waiter_username)
         except:
             waiter = None
-        
+
         new_order = Order.objects.create(
             customer_name=customer_name,
             item='Multiple Items',
@@ -107,11 +105,11 @@ def take_order(request, table_number):
             payment_method=payment_method,
             total_price=0
         )
-        
+
         total = 0
         item_ids = request.POST.getlist('item_ids')
         quantities = request.POST.getlist('quantities')
-        
+
         for item_id, qty in zip(item_ids, quantities):
             qty = int(qty)
             if qty > 0:
@@ -124,21 +122,25 @@ def take_order(request, table_number):
                     quantity=qty,
                     price=menu_item.price
                 )
-        
+                menu_item.daily_sold += qty
+                if menu_item.daily_limit > 0 and menu_item.daily_sold >= menu_item.daily_limit:
+                    menu_item.available = False
+                menu_item.save()
+
         new_order.total_price = total
         new_order.save()
-        
         table.is_busy = True
         table.save()
-        
+
         return redirect('receipt', pk=new_order.pk)
-    
+
     return render(request, 'menu/take_order.html', {
         'table': table,
         'categories': categories,
         'menu_items': menu_items,
         'waiter': waiter
     })
+
 def mark_table_free(request, table_number):
     table = Table.objects.get(number=table_number)
     table.is_busy = False
@@ -148,23 +150,16 @@ def mark_table_free(request, table_number):
 
 def daily_report(request):
     from django.utils import timezone
-    from django.db.models import Sum, Count
-    import datetime
-    
+    from django.db.models import Sum
+
     today = timezone.now().date()
-    
-    # Today's orders
     today_orders = Order.objects.filter(date__date=today)
-    
-    # Total money today
     total_money = today_orders.aggregate(Sum('total_price'))['total_price__sum'] or 0
-    
-    # Orders by payment method
     cash_total = today_orders.filter(payment_method='cash').aggregate(Sum('total_price'))['total_price__sum'] or 0
     zaad_total = today_orders.filter(payment_method='zaad').aggregate(Sum('total_price'))['total_price__sum'] or 0
     edahab_total = today_orders.filter(payment_method='edahab').aggregate(Sum('total_price'))['total_price__sum'] or 0
     mastercard_total = today_orders.filter(payment_method='mastercard').aggregate(Sum('total_price'))['total_price__sum'] or 0
-    
+
     return render(request, 'menu/daily_report.html', {
         'today_orders': today_orders,
         'total_money': total_money,
@@ -175,6 +170,7 @@ def daily_report(request):
         'mastercard_total': mastercard_total,
         'today': today,
     })
+
 def mark_item_finished(request, item_id):
     item = MenuItem.objects.get(id=item_id)
     item.available = False
